a2ad37e12eda93dbf8dda4d550614765bd3c665e
Christian Novalski <52799350+cnovalski1@users.noreply.github.com>
2024-06-03 15:49:06 -0400

    Implemented functions

--- acquire_project_data.py
+++ acquire_project_data.py
@@ -1,8 +1,9 @@
 from github import Github
 from datetime import datetime, timedelta, timezone
 import time
+import json
 
-# Function that checks the rate limit of the GitHub API to make sure that we don't hit the rate limit
+
 def rate_limit_check(g):
     rate_limit = g.get_rate_limit().core
     if rate_limit.remaining < 10:  
@@ -11,7 +12,6 @@ def rate_limit_check(g):
         sleep_duration = max(0, (rate_limit.reset - now).total_seconds() + 10)  # adding 10 seconds buffer
         time.sleep(sleep_duration)
 
-# Function that gets issues from a given repo and formats it as text
 def get_issue_text(g, repo, one_week_ago):
     issue_text = ""
     issues = repo.get_issues(state='all', since=one_week_ago)
@@ -28,26 +28,30 @@ def get_issue_text(g, repo, one_week_ago):
 
     return issue_text
 
-# Function that gets PRs from a given repo and formats it as text
+# Gets the text from pull requests, including title, body, and state.
 def get_pr_text(g, repo, one_week_ago):
     pr_text = ""
-    prs = repo.get_prs(state='all', since=one_week_ago)
-
-    for pr in prs:
+    pulls = repo.get_pulls(state='all', sort='created')
+    for pr in pulls:
         pr_text += f"Title: {pr.title}"
-        pr_text += f"Body: {pr.body}\n"
-
-        comments = pr.get_issue_comments()
-        for comment in comments:
-            pr_text += f"Comment by {comment.user.login}: {comment.body}\n"
-        pr_text += "--------------------------------------------------\n"
+        pr_text += f"Body: {pr.body}"
+        pr_text += f"State: {pr.state}\n"
+        pr_text += "--------------------------------------------------"
         rate_limit_check(g)
-
+    
     return pr_text
 
 def get_commit_messages(g, repo, one_week_ago):
-    ### TODO
-    return ""
+    commit_text = ""
+    commits = repo.get_commits(since=one_week_ago)
+
+    for commit in commits:
+        commit_text += f"Author: {commit.commit.author.name}"
+        commit_text += f"Message: {commit.commit.message}\n"
+        commit_text += "--------------------------------------------------"
+        rate_limit_check(g)
+
+    return commit_text
 
 
 if __name__ == '__main__':
@@ -59,7 +63,19 @@ if __name__ == '__main__':
     one_week_ago = datetime.now() - timedelta(days=7)
 
     print(get_issue_text(g, repo, one_week_ago))
+    print(get_pr_text(g, repo, one_week_ago))
+    print(get_commit_messages(g, repo, one_week_ago))
 
     # OUTPUT EVERYTHING AS A JSON FILE
+    #Sample JSON code
+    '''
+    dict = {
+     "issues": get_issue_text(g, repo, one_week_ago),
+     "pull requests": get_pr_text(g, repo, one_week_ago),
+     "commits": get_commit_messages(g, repo, one_week_ago)
+    }
+    with open("data.json", "w") as outfile:
+       json.dump(dict, outfile)
+    '''
 
     g.close()

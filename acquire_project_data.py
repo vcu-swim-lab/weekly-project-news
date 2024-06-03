b3c58e90a81019316ae05d80ffd22e9d557eb7db
stevenbui44 <stevenbui44@gmail.com>
2024-06-03 12:00:25 -0400

    Wrote code for get_pr_text

--- acquire_project_data.py
+++ acquire_project_data.py
@@ -2,7 +2,7 @@ from github import Github
 from datetime import datetime, timedelta, timezone
 import time
 
-
+# Function that checks the rate limit of the GitHub API to make sure that we don't hit the rate limit
 def rate_limit_check(g):
     rate_limit = g.get_rate_limit().core
     if rate_limit.remaining < 10:  
@@ -11,6 +11,7 @@ def rate_limit_check(g):
         sleep_duration = max(0, (rate_limit.reset - now).total_seconds() + 10)  # adding 10 seconds buffer
         time.sleep(sleep_duration)
 
+# Function that gets issues from a given repo and formats it as text
 def get_issue_text(g, repo, one_week_ago):
     issue_text = ""
     issues = repo.get_issues(state='all', since=one_week_ago)
@@ -27,9 +28,22 @@ def get_issue_text(g, repo, one_week_ago):
 
     return issue_text
 
+# Function that gets PRs from a given repo and formats it as text
 def get_pr_text(g, repo, one_week_ago):
-    ### TODO
-    return ""
+    pr_text = ""
+    prs = repo.get_prs(state='all', since=one_week_ago)
+
+    for pr in prs:
+        pr_text += f"Title: {pr.title}"
+        pr_text += f"Body: {pr.body}\n"
+
+        comments = pr.get_issue_comments()
+        for comment in comments:
+            pr_text += f"Comment by {comment.user.login}: {comment.body}\n"
+        pr_text += "--------------------------------------------------\n"
+        rate_limit_check(g)
+
+    return pr_text
 
 def get_commit_messages(g, repo, one_week_ago):
     ### TODO
--- download_new_subscribers.py
+++ download_new_subscribers.py
@@ -2,7 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  
+load_dotenv('public.env')  
 
 headers = {
     "Authorization": f"Token {os.environ['BUTTONDOWN_API_KEY']}",
@@ -15,3 +15,44 @@ METHOD = "GET"
 response = requests.request(METHOD, f"{BASE_URL}/v1{ENDPOINT}", headers=headers)
 
 print(response.text)
+
+# {
+#   "results": [
+#     {
+#       "id": "e460635d-bd38-40c4-beb5-d4d635362d5f",
+#       "email": "kostadin@gmail.com",                                              # email submitted from the buttondown website
+#       "notes": "", 
+#       "metadata": {                                                               # github repo submitted from the buttondown website
+#         "repo_name": "https://github.com/tensorflow/tensorflow"
+#       },
+#       "tags": [],
+#       "referrer_url": "https://buttondown.email/weekly-project-news",             # the buttondown website
+#       "creation_date": "2024-05-16T19:23:06.309307Z",
+#       "secondary_id": 1,
+#       "subscriber_type": "regular",
+#       "source": "api",
+#       "utm_campaign": "",
+#       "utm_medium": "",
+#       "utm_source": "",
+#       "referral_code": "whzzgj",
+#       "avatar_url": "https://s.gravatar.com/avatar/605b50cc1af4045b13b866570149778a?s=100&d=404",
+#       "stripe_customer_id": null,
+#       "stripe_coupon": null,
+#       "unsubscription_date": null,
+#       "churn_date": null,
+#       "unsubscription_reason": "",
+#       "transitions": [
+#         {
+#           "date": "2024-05-16T19:23:25.068217Z",
+#           "type": "regular"
+#         }
+#       ],
+#       "ip_address": "128.172.49.129",
+#       "last_open_date": null,
+#       "last_click_date": null
+#     }
+#   ],
+#   "next": null,
+#   "previous": null,
+#   "count": 1
+# }

f2764a888b8080c460fd4313c78a41088c4aac42
stevenbui44 <stevenbui44@gmail.com>
2024-06-10 11:09:46 -0400

    Got LLMChain to work instead of ConversationChain

--- create_newsletter.py
+++ create_newsletter.py
@@ -4,6 +4,10 @@ from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
 
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI 
+from langchain.prompts import PromptTemplate
+
 load_dotenv('public.env')  
 
 API_KEY = os.environ.get("OPENAI_KEY")
@@ -17,20 +21,36 @@ llm = ChatOpenAI(
 
 # Function that generates a summary of the project's PRs
 # https://stackoverflow.com/questions/77316112/langchain-how-do-input-variables-work-in-particular-how-is-context-replaced
-# def generate_pull_requests_summary(context, question):
-def generate_pull_requests_summary(prompt):
+def generate_pull_requests_summary(context, question):
+# def generate_pull_requests_summary(prompt):
   
   # use a ConversationChain because LLMChain is deprecated
   # establishes a conversation connection
-  conversation = ConversationChain(llm=llm, verbose=True)
+  # conversation = ConversationChain(llm=llm, verbose=True)
 
-  # print('context: ', context)
-  # print('question: ', question)
+  print('a')
+  print('Context: ', context)
+  print('Question: ', question)
+  print('b')
+
+
+  prompt_template = "Context: {context}\nQuestion: {question}"
+  PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+  print('c')
+  print('Prompt: ', PROMPT)
+  print('d')
+
+  chain = LLMChain(llm=OpenAI(temperature=1.0, openai_api_key = os.environ.get("OPENAI_KEY")), prompt=PROMPT)
+
+  # response = chain.invoke(question=question, input=PROMPT)
+  response = chain.invoke({"context": context, "question": question})
 
   # use invoke because run is deprecated and takes only 1 argument
   # generates a response to an input question
   # response = conversation.invoke(input=question, context=context)
-  response = conversation.predict(input=prompt)
+  # response = conversation.predict(input=prompt)
+  # response = "not yet"
 
   return response;
 
@@ -41,7 +61,7 @@ if __name__ == '__main__':
 
   for repo in github_data:
 
-    # case 1: there is np data (aka json field is [])
+    # case 1: there is no data (aka json field is [])
     if repo.get("num_all_open_issues") is None:
       context = "No data available."
     # case 2: there is data
@@ -58,14 +78,11 @@ if __name__ == '__main__':
     # This is what the prompt looks like:
     # "Context :26
     # Question: Generate 10 words or fewer summarizing this data, representing the number of open issue"
-    prompt = f"Context: {context}\nQuestion: {question}"
+    # prompt = f"Context: {context}\nQuestion: {question}"
 
-    # response = generate_pull_requests_summary(context, question)
-    response = generate_pull_requests_summary(prompt)
+    response = generate_pull_requests_summary(context, question)
+    # response = generate_pull_requests_summary(prompt)
 
   print('Response:')
 
   print(response)
-
-  # with open('newsletter_data.json', 'w') as file: 
-  #   json.dump(github_data, file, indent=2)

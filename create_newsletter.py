789e102e636457d33d525475e9ea7dbbe42bbd2b
stevenbui44 <stevenbui44@gmail.com>
2024-06-14 14:21:49 -0400

    Tested generating output with 4o and claude but ran into error code 429 (exceeded quota)

--- create_newsletter.py
+++ create_newsletter.py
@@ -12,153 +12,80 @@ API_KEY = os.environ.get("OPENAI_KEY")
 # https://stackoverflow.com/questions/77316112/langchain-how-do-input-variables-work-in-particular-how-is-context-replaced
 prompt_template = "Instructions: {instructions}\nJSON data: {data}\n"
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["instructions", "data"])
+llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key = API_KEY, max_tokens=50)
 # llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key = API_KEY)
-llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key = API_KEY, max_tokens=10)
 chain = PROMPT | llm
 
 
 # Generates the summary using ChatGPT given any context and question (prompt template above)
 def generate_summary(instructions, data):
     response = chain.invoke({"instructions": instructions, "data": data})
+
+    input_tokens = response.runnables[0].input_tokens
+    output_tokens = response.runnables[1].output_tokens
+    print(f"Input Tokens: {input_tokens}")
+    print(f"Output Tokens: {output_tokens}")
+
     return response.content
 
 
-# # ISSUES 1
-# def summary_issues_open(repo):
-#   context = "No open issues." if not repo.get("issues_open") else json.dumps(repo["issues_open"], indent=2).strip()
-#   question = "Generate 10 words or fewer summarizing this data, representing open issues in a GitHub repository"
-#   return generate_summary(context, question)
+# ISSUES - Stastics
+def issues_statistics(repo):
+  instructions = """You are a GitHub project maintainer writing a newsletter that gives a summary of your project's issues, pull requests, commits, contributors, and statistics. 
+  
+  Create the newsletter section describing all of the total open issues, issues opened this week, and issues closed this week using the JSON data below. The output should be informational, descriptive, and only based on the JSON data given (numbers). If you do not know something, say you do not know instead of making something up. 
+  
+  Follow this structure: 
+  Header: "Statistics" 
+  Subheading: "Total Open Issues" 
+  A bullet point with 2-3 sentences giving a description of the total open issues this week. 
+  Subheading: "Issues Opened This Week" 
+  A bullet point with 2-3 sentences giving a description of the issues opened this week. 
+  Subheading: "Issues Closed This Week" 
+  A bullet point with 2-3 sentences giving a description of the issues closed this week. 
+  Subheading: “All Open Issues”
+  A bullet point with 2-3 sentences giving a description of each open issue based on comments, body, and issue title. If there are more than 10 issues, cluster some bullet points together so there are at most 10 bullet points.
+  
+  Show the output with markdown tags in a code block. Do not just give regular output."""
 
-# # ISSUES 2
-# def summary_issues_closed(repo):
-#   context = "No closed issues." if not repo.get("issues_closed") else json.dumps(repo["issues_closed"], indent=2).strip()
-#   question = "Generate 10 words or fewer summarizing this data, representing closed issues in a GitHub repository"
-#   return generate_summary(context, question)
+  data = {
+    "num_all_open_issues": repo['num_all_open_issues'],
+    "num_weekly_open_issues": repo['num_weekly_open_issues'],
+    "num_weekly_closed_issues": repo['num_weekly_closed_issues'],
+    "open_issues": repo['open_issues']
+  }
 
-# # ISSUES 3
-# def summary_num_all_open_issues(repo):
-#   context = "No open issues." if not repo.get("num_all_open_issues") else json.dumps(repo["num_all_open_issues"], indent=2).strip()
-#   question = "Generate 10 words or fewer summarizing this data, representing the total number of open issues in a GitHub repository"
-#   return generate_summary(context, question)
+  return generate_summary(instructions, data).strip()
 
-# # ISSUES 4
-# def summary_num_weekly_open_issues(repo):
-#   context = "No open issues made this week." if not repo.get("num_weekly_open_issues") else json.dumps(repo["num_weekly_open_issues"], indent=2).strip()
-#   question = "Generate 10 words or fewer summarizing this data, representing the number of new open issues made this week in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # ISSUES 5
-# def summary_num_weekly_closed_issues(repo):
-#   context = "No issues closed this week." if not repo.get("num_weekly_closed_issues") else json.dumps(repo["num_weekly_closed_issues"], indent=2).strip()
-#   question = "Generate 10 words or fewer summarizing this data, representing the number of issues closed this week in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # ISSUES 6
-# def summary_issues_by_open_date(repo):
-#   context = "No open issues." if not repo.get("issues_by_open_date") else json.dumps(repo["issues_by_open_date"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the open issues in a GitHub repository sorted by their time open"
-#   return generate_summary(context, question)
-
-# # ISSUES 7
-# def summary_issues_by_number_of_comments(repo):
-#   context = "No open issues." if not repo.get("issues_by_number_of_comments") else json.dumps(repo["issues_by_number_of_comments"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the open issues in a GitHub repository sorted by their number of comments"
-#   return generate_summary(context, question)
-
-# # ISSUES 8
-# def summary_avg_issue_close_time(repo):
-#   context = "No open issues." if not repo.get("average_issue_close_time") else json.dumps(repo["average_issue_close_time"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the average time it takes for an issue to close in a GitHub repository, regardless of its creation date"
-#   return generate_summary(context, question)
-
-# # ISSUES 9
-# def summary_avg_issue_close_time_weekly(repo):
-#   context = "No open issues." if not repo.get("average_issue_close_time_weekly") else json.dumps(repo["average_issue_close_time_weekly"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the average time it took for issues to close this week in a GitHub repository"
-#   return generate_summary(context, question)
-
-
-
-# # PRS 1
-# def summary_open_pull_requests(repo):
-#   context = "No open pull requests." if not repo.get("open_pull_requests") else json.dumps(repo["open_pull_requests"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the open pull requests in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # PRS 2
-# def summary_closed_pull_requests(repo):
-#   context = "No closed pull requests." if not repo.get("closed_pull_requests") else json.dumps(repo["closed_pull_requests"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the closed pull requests in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # PRS 3
-# def summary_num_all_prs(repo):
-#   context = "No pull requests." if not repo.get("num_all_prs") else json.dumps(repo["num_all_prs"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of open and closed pull requests in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # PRS 4
-# def summary_num_open_prs(repo):
-#   context = "No open pull requests." if not repo.get("num_open_prs") else json.dumps(repo["num_open_prs"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of open requests in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # PRS 5
-# def summary_num_closed_prs(repo):
-#   context = "No closed pull requests." if not repo.get("num_closed_prs") else json.dumps(repo["num_closed_prs"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of closed requests in a GitHub repository"
-#   return generate_summary(context, question)
-
-
-
-# # COMMITS 1
-# def summary_commits(repo):
-#   context = "No commits." if not repo.get("commits") else json.dumps(repo["commits"], indent=2).strip()
-#   question = "Summarize the content of this data, representing all of the commits in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # COMMITS 2
-# def summary_num_commits(repo):
-#   context = "No commits." if not repo.get("num_commits") else json.dumps(repo["num_commits"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of commits in a GitHub repository"
-#   return generate_summary(context, question)
-
-
-
-# # CONTRIBUTORS 1
-# def summary_new_contributors(repo):
-#   context = "No new contributors this week." if not repo.get("new_contributors") else json.dumps(repo["new_contributors"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of new contributors in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # CONTRIBUTORS 2
-# def summary_contributed_this_week(repo):
-#   context = "No contributors this week." if not repo.get("contributed_this_week") else json.dumps(repo["contributed_this_week"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of contributors this week in a GitHub repository"
-#   return generate_summary(context, question)
-
-# # CONTRIBUTORS 3
-# def summary_active_contributors(repo):
-#   context = "No active contributors." if not repo.get("active_contributors") else json.dumps(repo["active_contributors"], indent=2).strip()
-#   question = "Summarize the content of this data, representing the number of active contributors in a GitHub repository"
-#   return generate_summary(context, question)
 
 
 
 
 def test(repo):
   instructions = """You are a GitHub project maintainer writing a newsletter that gives a summary of your project's issues, pull requests, commits, contributors, and statistics. 
+Create the newsletter section describing all of the new contributors this week, total contributors this week, and active contributors in the project using the JSON data below. The output should be informational, descriptive, and only based on the JSON data given (numbers of contributors). If you do not know something, say you do not know instead of making something up. 
 
-Create the newsletter section describing all of the closed pull requests this week using the JSON data below. The output should be informational, descriptive, and only based on the JSON data given (title, body). If you do not know something, say you do not know instead of making something up. 
-    
 Follow this structure: 
-Use '## Closed PRs' as the main header. 
-Use '### PR: ' + (title of pull request) for each subheading. 
-Use a bullet point with 2-3 sentences giving a description of each closed PR. If there are more than 10 PRs, cluster some PRs together so there are at most 10 bullet points.
 
-Output should be generated with markdown tags. Numbers in the output should be in bold too."""
+Use '## Contributors' as the main header. 
+Use '### New Contributors’ for the subheading. 
+A bullet point with 2-3 sentences giving a description of the number of new contributors this week. 
+A bullet point listing some of the new contributors. This should only be one bullet point in total, and you do not need to name all new contributors.
+Use '### Weekly Contributors’ for the subheading. 
+A bullet point with 2-3 sentences giving a description of the number of total contributors this week. 
+A bullet point listing some of the contributors. This should only be one bullet point in total, and you do not need to name all contributors.
+Use '### Active Contributors’ for the subheading. 
+A bullet point with 2-3 sentences giving a description of the number of active contributors in the project. 
+A bullet point listing some of the active contributors. This should only be one bullet point in total, and you do not need to name all contributors.
 
-  data = repo['closed_pull_requests']
+Show the output with markdown tags in a code block. Do not just give regular output.
+"""
+
+  data = {
+    "new_contributors": repo['new_contributors'],
+    "contributed_this_week": repo['contributed_this_week'],
+    "active_contributors": repo['active_contributors']
+  }
 
   return generate_summary(instructions, data).strip()
 
@@ -204,32 +131,13 @@ if __name__ == '__main__':
             result = test(repo)
             outfile.write(result)
 
+            # ISSUES: Statistics
+            # issues_statistics = issues_statistics(repo)
+            # outfile.write(issues_statistics)
+
+
+
           print(f"Successfully added {project_name} to {newsletter_filename}")
         except Exception as e:
           print(f"Error writing {project_name} to {newsletter_filename}")
           print(f"Error code: {e}")
-
-# summaries = {
-  # "summaries_issues_open": summary_issues_open(repo),
-  # "summaries_issues_closed": summary_issues_closed(repo),
-  # "summaries_num_all_open_issues": summary_num_all_open_issues(repo),
-  # "summaries_num_weekly_open_issues": summary_num_weekly_open_issues(repo),
-  # "summaries_num_weekly_closed_issues": summary_num_weekly_closed_issues(repo),
-  # "summaries_issues_by_open_date": summary_issues_by_open_date(repo),
-  # "summaries_issues_by_number_of_comments": summary_issues_by_number_of_comments(repo),
-  # "summaries_avg_issue_close_time": summary_avg_issue_close_time(repo),
-  # "summaries_avg_issue_close_time_weekly": summary_avg_issue_close_time_weekly(repo),
-
-  # "summaries_open_pull_requests": summary_open_pull_requests(repo),
-  # "summaries_closed_pull_requests": summary_closed_pull_requests(repo),
-  # "summaries_num_all_prs": summary_num_all_prs(repo),
-  # "summaries_num_open_prs": summary_num_open_prs(repo),
-  # "summaries_num_closed_prs": summary_num_closed_prs(repo),
-
-  # "summaries_commits": summary_commits(repo),
-  # "summaries_num_commits": summary_num_commits(repo),
-
-  # "summaries_new_contributors": summary_new_contributors(repo),
-  # "summaries_contributed_this_week": summary_contributed_this_week(repo),
-  # "summaries_active_contributors": summary_active_contributors(repo)
-# }

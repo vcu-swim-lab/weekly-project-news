411059761631db5b40593d619075203765647328
Imran <imranraad@gmail.com>
2024-06-19 22:59:15 +0000

    issue parse added

--- github_data_parse_sample.py
+++ github_data_parse_sample.py
@@ -66,7 +66,110 @@ repo = 'APIexample'
 repo_data = get_a_repository(owner, repo)
 insert_repository(repo_data)
 
-owner = 'ggerganov'
-repo = 'llama.cpp'
+owner = 'danny-avila'
+repo = 'LibreChat'
 repo_data = get_a_repository(owner, repo)
 insert_repository(repo_data)
+
+
+def get_issues(repo_owner, repo_name):
+    issues_array = []
+    page = 1
+
+    while True:
+        url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues"
+        params = {
+            'state': 'all',
+            'page': page,
+            'per_page': 100
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        print(response)
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch issues: {response.status_code}")
+
+        page_issues = response.json()
+        if not page_issues:
+            break
+
+        issues_array.extend(page_issues)
+        page += 1
+
+    return issues_array
+
+
+def insert_user(data): 
+    repository_fields = {column.name for column in User.__table__.columns}
+    filtered_data = {key: value for key, value in data.items() if key in repository_fields}
+
+    # Create and add the repository to the session
+    new_user = User(**filtered_data)
+    session.add(new_user)
+    session.commit()
+
+
+# Define a function to load issue data from a JSON file
+def insert_issue(data):
+    # Extract only the fields that exist in the Issue model
+    issue_fields = {column.name for column in Issue.__table__.columns}
+    filtered_data = {key: value for key, value in data.items() if key in issue_fields}
+
+    if session.query(Issue).filter_by(id=filtered_data['id']).first() is not None:
+        print("Already Exists!")
+        return
+
+    if 'user' in data:
+        user_data = data['user']
+        user = session.query(User).filter_by(login=user_data['login']).first()
+        if not user:
+            insert_user(user_data)
+        user = session.query(User).filter_by(login=user_data['login']).first()
+        filtered_data['user_login'] = user.login
+
+    if 'repository_url' in data:
+        repository = session.query(Repository).filter_by(url=data['repository_url']).first()
+        filtered_data['repository_full_name'] = repository.full_name
+
+
+    if 'labels' in data:
+        labels_data = data['labels']
+        labels = []
+        for label_data in labels_data:
+            label = session.query(Label).filter_by(name=label_data['name']).first()
+            if not label:
+                label = Label(name=label_data['name'],)
+                session.add(label)
+                session.commit()
+            labels.append(label)
+        filtered_data['labels'] = labels
+
+    # Convert datetime fields
+    datetime_fields = ['created_at', 'updated_at', 'closed_at']
+    for field in datetime_fields:
+        if field in filtered_data:
+            if filtered_data[field] is None:
+                filtered_data[field] = None
+            else:
+                filtered_data[field] = datetime.strptime(filtered_data[field], "%Y-%m-%dT%H:%M:%SZ")
+
+    # Create and add the issue to the session
+    new_issue = Issue(**filtered_data)
+    session.add(new_issue)
+    session.commit()
+
+
+# Example usage
+owner = 'cnovalski1'
+repo = 'APIexample'
+issues_array = get_issues(owner, repo)
+for issue in issues_array:
+    insert_issue(issue)
+
+
+# Example usage
+owner = 'danny-avila'
+repo = 'LibreChat'
+issues_array = get_issues(owner, repo)
+for issue in issues_array:
+    insert_issue(issue)

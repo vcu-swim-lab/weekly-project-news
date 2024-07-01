4e4fbde9c62f657c22baf8ded5a7dd02d736e062
cnovalski1 <christiannovalski@gmail.com>
2024-07-01 15:32:59 -0400

    Updated files

--- tables/issue.py
+++ tables/issue.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Bool
 from sqlalchemy.orm import relationship
 from .base import Base
 from .user import User
-from .label import Label
 from .repository import Repository
 
 class Issue(Base):
@@ -16,8 +15,6 @@ class Issue(Base):
     state = Column(String, nullable=False)
     title = Column(String, nullable=False)
     body = Column(Text)
-    locked = Column(Boolean)
-    active_lock_reason = Column(String)
     comments = Column(Integer)
     closed_at = Column(DateTime)
     created_at = Column(DateTime)
@@ -25,16 +22,9 @@ class Issue(Base):
 
     user_login = Column(String)
     repository_full_name = Column(String)
-
-    labels = relationship("Label", secondary="issue_labels")
-
+    
     def __repr__(self):
         return f"<Issue(title={self.title}, state={self.state})>"
-
-class IssueLabel(Base):
-    __tablename__ = 'issue_labels'
-    issue_id = Column(Integer, ForeignKey('issues.id'), primary_key=True)
-    label_name = Column(Integer, ForeignKey('labels.name'), primary_key=True)
 
 class IssueComment(Base):
     __tablename__ = 'issue_comments'

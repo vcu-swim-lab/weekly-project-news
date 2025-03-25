8d0a8b5e5300f25d965906819e69fd1d3d1da5e2
Christian Novalski <christiannovalski@gmail.com>
2025-03-25 11:34:35 -0400

    Fixing send bug

--- send_newsletter.py
+++ send_newsletter.py
@@ -18,34 +18,24 @@ load_dotenv()
 headers = {
     "Authorization": f"Token {os.environ['BUTTONDOWN_API_KEY']}",
 }
-BASE_URL = "https://api.buttondown.email"
+BASE_URL = "https://api.buttondown.com"
 
 
 # Function 1
 # POST /emails: DRAFT an email, not sending it out yet but getting an email id
 def draft_email(subject, content):
-    print("Starting to draft email")
-    # Check if email has already been drafted
-    # if subject in drafts:
-    #     logging.info(f"Reusing existing draft email for subject: {subject}")
-    #     print(f"Reusing existing draft email for subject: {subject}")
-    #     return {"id": drafts[subject]}  # Return the existing email ID
-
     url = f"{BASE_URL}/v1/emails"
     data = {
         "subject": subject,
         "body": content,
-        "status": "about_to_send"
+        "status": "draft"
     }
 
     response = requests.post(url, headers=headers, json=data)
 
     if response.status_code >= 200 and response.status_code < 300:
         response_data = response.json()
-        drafts[subject] = response_data['id']
         logging.info(f"Successfully drafted email with subject: {subject}")
-        print(f"Successfully drafted email with subject: {subject}. Printing response")
-        print(response_data)
         return response_data
     else:
         print("\nError in draft_email()")
@@ -58,63 +48,70 @@ def draft_email(subject, content):
 # Function 2
 # POST /subscribers/{id_or_email}/emails/{email_id}: Send existing email to subscriber
 def send_email_to_subscriber(subscriber_id, email_id):
-    print("Starting sending email process")
     url = f"{BASE_URL}/v1/subscribers/{subscriber_id}/emails/{email_id}"
-   
-    response = requests.post(url, headers=headers)
-    if response.status_code >= 200 and response.status_code < 300:
+    
+    print(f"Sending email {email_id} to subscriber {subscriber_id}")
+    logging.info(f"Attempting to send email (ID: {email_id}) to subscriber (ID: {subscriber_id})")
 
-        # change email status from "about_to_send" to "sent"
-        update_response = update_email_status(email_id)
-        if update_response.status_code >= 200 and update_response.status_code < 300:
-            logging.info(f"Successfully sent email (ID: {email_id}) to subscriber (ID: {subscriber_id})")
-            print(f"Successfully sent email (ID: {email_id}) to subscriber (ID: {subscriber_id})")
+    response = requests.post(url, headers=headers)
+    
+    print(f"Sending response: {response.status_code}")
+    
+    if response.status_code >= 200 and response.status_code < 300:
+        print("Successfully sent email")
+        logging.info(f"Email (ID: {email_id}) successfully sent to subscriber (ID: {subscriber_id})")
+
+        # Update email status from "about_to_send" to "imported"
+        update_response = update_email_status(email_id, "imported")
+        if update_response and update_response.status_code >= 200 and update_response.status_code < 300:
+            print(f"Successfully updated email (ID: {email_id}) status to 'about_to_send'")
+            logging.info(f"Successfully updated email (ID: {email_id}) status to 'about_to_send'")
             return response
         else:
-            print("\nError in send_email_to_subscriber()")
-            print(f"Failed to send email. Status code: {response.status_code}")
-            print(f"Response: {response.text}\n")
+            print("\nError updating email status after sending")
+            print(f"Failed to update email status for email ID: {email_id}")
             logging.error(f"Failed to update email status for email ID: {email_id}")
             return None
     else:
         print("\nError in send_email_to_subscriber()")
         print(f"Failed to send email. Status code: {response.status_code}")
         print(f"Response: {response.text}\n")
-        logging.error(f"Failed to send email to subscriber. Status code: {response.status_code}, Response: {response.text}")
+        logging.error(f"Failed to send email to subscriber (ID: {subscriber_id}). Status code: {response.status_code}, Response: {response.text}")
         return None
     
 
 # Function 3: 
 # PATCH /emails/{email_id}: Update email status to "sent"
-def update_email_status(email_id):
-    print("Starting updating email process")
+
+def update_email_status(email_id, status, max_retries=3, delay=5):
     url = f"{BASE_URL}/v1/emails/{email_id}"
     data = {
-        "subject": subject,
-        "body": content,
-        "status": "imported"
+        "status": {status}
     }
-    
-    response = requests.patch(url, headers=headers, json=data)
 
-    if response.status_code >= 200 and response.status_code < 300:
-        logging.info(f"Successfully updated email (ID: {email_id}) status to 'imported'")
-        print(f"Successfully updated email (ID: {email_id}) status to 'imported'")
-        return response
-    else:
-        print(f"\nError in update_email_status() - Status code: {response.status_code}")
+    for attempt in range(1, max_retries + 1):
+        response = requests.patch(url, headers=headers, json=data)
+
+        if response.status_code >= 200 and response.status_code < 300:
+            logging.info(f"Successfully updated email (ID: {email_id}) status to 'about_to_send'")
+            return response
+
+        print(f"\nAttempt {attempt} - Error in update_email_status() - Status code: {response.status_code}")
         print(f"Response: {response.text}\n")
-        logging.error(f"Error in update_email_status() - Status code: {response.status_code}, Response: {response.text}")
-        return None
+        logging.error(f"Attempt {attempt} - Error in update_email_status() - Status code: {response.status_code}, Response: {response.text}")
+
+        if attempt < max_retries:
+            time.sleep(delay)  # Wait before retrying
+        else:
+            logging.error(f"Failed to update email status after {max_retries} attempts.")
+            return None
+
 
     
 
 # "Main function"
 logging.info("- - - - - - - - - - - - - - - - - - - - -")
 logging.info("Starting newsletter sending process")
-
-# List to hold drafts
-drafts = {}
 
 with open('subscribers.json', 'r') as file:
     subscribers_data = json.load(file)
@@ -125,10 +122,10 @@ for subscriber in subscribers_data['results']:
         # print(error_message)
         # logging.error(error_message)
         continue
-    email = subscriber['email']
-    # print('VALID: ', email)
 
-    # For pushing to server, uncomment this block
+    email = subscriber['email']
+    print('VALID: ', email)
+
     github_repo = subscriber.get('metadata', {}).get('repo_name')
     if not github_repo:
         print(f"No repo found for: {email}")
@@ -136,36 +133,44 @@ for subscriber in subscribers_data['results']:
 
     # STEP 1: get the subscriber's markdown file from the folder
     project_name = github_repo.split('github.com/')[-1].replace('/', '_')
-    
     newsletter_filepath = f"newsletter_data/newsletter_{project_name}.txt"
     if not os.path.exists(newsletter_filepath):
         print(f"No newsletter txt file found: {newsletter_filepath}")
         continue
 
     # STEP 2: get the content for the email
-    with open(newsletter_filepath, 'r', encoding='utf-8') as newsletter_file:
+    with open(newsletter_filepath, 'r') as newsletter_file:
         content = newsletter_file.read()
-        print(f"Content read")
 
     # STEP 3: get the subject for the email
     name = github_repo.split('/')[-1]
     capitalized_name = name[0].upper() + name[1:]
+    timestamp = datetime.now().strftime('%H:%M:%S')
     timestamp_from = (datetime.now() - timedelta(days=7)).strftime('%B %d, %Y')
     timestamp_to = datetime.now().strftime('%B %d, %Y')
-    subject = f"Weekly GitHub Report for {capitalized_name}: {timestamp_from} - {timestamp_to} for ({email})"
+    subject = f"Weekly GitHub Report for {capitalized_name}: {timestamp_from} - {timestamp_to} ({timestamp})"
 
     # STEP 4: DRAFT the email using the content and subject to get an email ID (NOT sending it yet)
     response = draft_email(subject, content)
-    print(f"Printing draft: {response}")
+    print(f"Response for drafting email for {email}: {response}")
     if not response:
         print(f"Draft email not able to be made for: {email}")
-        logging.error(f"Draft email not able to be made for: {email}, no response")
+        logging.error(f"Draft email not able to be made for: {email}")
         continue
 
-    # STEP 5: SEND the email to the subscriber
+
+    # STEP 5: Get the email ID and subscriber ID
     email_id = response['id']
     subscriber_id = subscriber['id']
-    print(f"Sending email {email_id} to subscriber {subscriber_id} ({email})")
+
+    # STEP 6: Update email status to "about_to_send"
+    update_response = update_email_status(email_id, "about_to_send")
+    if not update_response:
+        print(f"Failed to update email status for: {email}")
+        logging.error(f"Failed to update email status for: {email}")
+        continue
+
+    # STEP 7: Send the email to the subscriber
     send_response = send_email_to_subscriber(subscriber_id, email_id)
     if send_response:
         print(f"Email sent to subscriber: {email}")

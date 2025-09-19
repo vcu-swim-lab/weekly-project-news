2696f33d45fb7295a9169fa70c25b15697da86fa
christochacha <chavezci@vcu.edu>
2025-09-19 11:57:24 -0400

    fix starting message

--- clean_db.py
+++ clean_db.py
@@ -1,6 +1,7 @@
-# This file updates existing pull requests, issues, comments, version information, etc. in the database
-# It avoids the need to delete and recreate the entire database each week
-# This script significantly improves performance and efficiency and it scales much better
+# This file cleans the database by removing outdated or unnecessary information
+# It deletes stale issues and pull requests that are no longer relevant
+# It also removes repositories and other entries that are no longer active/needed
+# The purpose is to keep the database lean and up to date
 
 from github import Github
 from datetime import datetime, timedelta, timezone
